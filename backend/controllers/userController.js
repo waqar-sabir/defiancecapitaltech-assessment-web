965692cec29d917404540eba9818cc9627c16593
waqar-sabir <waqarsabir94@gmail.com>
2026-04-10 02:31:01 +0500

    Task1 completed!

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,10 @@ const loginUser = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.json({ success: false, message: "Invalid credentials" });
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
     }
 
     const token = createToken(user._id);
@@ -38,7 +41,10 @@ const registerUser = async (req, res) => {
     // checking if user already exists
     const exists = await userModel.findOne({ email });
     if (exists) {
-      return res.json({ success: false, message: "User already exists" });
+      return res.status(401).json({
+        success: false,
+        message: "User already exists",
+      });
     }
 
     // validating email format and password
